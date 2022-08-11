1456ac7bddd1290811d6e3eda5e2b18bb73abd0d
John Doe <darlanebrown@gmail.com>
2022-08-10 17:17:40 -0700

    Updates

---
+++ precache-manifest.54d6cf6ca2cff5a6741661ddc4b95ed1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4f7bdba8b5aac78ab82a5417bc4d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e620cb2237f5ed01ad57",
+    "url": "/static/css/2.8dfc1fa8.chunk.css"
+  },
+  {
+    "revision": "67017857fbd717ebb932",
+    "url": "/static/css/main.b95ad5dd.chunk.css"
+  },
+  {
+    "revision": "e620cb2237f5ed01ad57",
+    "url": "/static/js/2.bb955253.chunk.js"
+  },
+  {
+    "revision": "1fd3ae49c279980f958569bed4f64a14",
+    "url": "/static/js/2.bb955253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584fc349d50ea6f9ca25",
+    "url": "/static/js/3.6a2da8c5.chunk.js"
+  },
+  {
+    "revision": "67017857fbd717ebb932",
+    "url": "/static/js/main.cc7d1444.chunk.js"
+  },
+  {
+    "revision": "6cacc2e37085b8898f23",
+    "url": "/static/js/runtime-main.4fad3418.js"
+  },
+  {
+    "revision": "e4608424f9c186795c8c3a20e2a7b42a",
+    "url": "/static/media/Darla B.e4608424.jpg"
+  },
+  {
+    "revision": "b520d85731acce4113f1c0c5b13a5c96",
+    "url": "/static/media/background.b520d857.jpeg"
+  },
+  {
+    "revision": "4a0d64de7d1a47177141b1230b5cf2d3",
+    "url": "/static/media/portfolio-1.4a0d64de.png"
+  },
+  {
+    "revision": "e51973b2c4f1178680787f307bd6a8f2",
+    "url": "/static/media/portfolio-2.e51973b2.png"
+  },
+  {
+    "revision": "b42e6e9f38ec7cb8e8f0be418bd03885",
+    "url": "/static/media/portfolio-3.b42e6e9f.png"
+  },
+  {
+    "revision": "90c57d8f66f3dc4b34a9ed1201a95b95",
+    "url": "/static/media/portfolio-4.90c57d8f.png"
+  },
+  {
+    "revision": "d873c67eeb9dd96ffdb48fb59ddc1bf5",
+    "url": "/static/media/portfolio-5.d873c67e.png"
+  },
+  {
+    "revision": "6148bdd9dceb3c639569200dd4a62ed3",
+    "url": "/static/media/portfolio-6.6148bdd9.png"
+  },
+  {
+    "revision": "6b4dab1f44341c518b098ce030b8f563",
+    "url": "/static/media/portfolio-7.6b4dab1f.png"
+  },
+  {
+    "revision": "c1e239d639f8f43f41ced6efd6174008",
+    "url": "/static/media/portfolio-8.c1e239d6.png"
+  },
+  {
+    "revision": "c3b85a456c5b800203c0bcb11f648062",
+    "url": "/static/media/portfolio-9.c3b85a45.png"
+  },
+  {
+    "revision": "a36ab35dff9d24695fbea24ed209bc69",
+    "url": "/static/media/portfolio-bg.a36ab35d.jpg"
+  }
+]);
